571e63a847df1480af4ba1e5f5ca88fd6ec98b75
Kaapeli <kaapo.immonen@gmail.com>
2025-01-24 21:46:12 +0200

    Remove data_loader.py as it is deprecated

--- src/data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(filepath):
-    try:
-        data = pd.read_csv(filepath, delimiter=';')
-        return data
-    except pd.errors.ParserError as e:
-        print(f"Error parsing {filepath}: {e}")
-        return None
