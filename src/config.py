2176f423cb56ba065074c60a998953e3fe282205
Kaapeli <kaapo.immonen@gmail.com>
2025-01-25 18:20:50 +0200

    add heatmap parameters

--- src/config.py
+++ src/config.py
@@ -14,15 +14,18 @@ SCORES = 'scores.json'
 
 # Preprocessing
 PREPROCESS = False # Preprocess the data?
-NUMBER_OF_ROWS = 50 # Limit number of rows to preprocess (0 for all rows)
+NUMBER_OF_ROWS = 100 # Limit number of rows to preprocess (0 for all rows)
 TWO_COLUMNS = True # Combine two columns into one? Set False if dataset has only one column and label
 DATA_COLUMN_NAME_1 = 'title' # Data column name for the first column
 DATA_COLUMN_NAME_2 = 'text' # Data column name for the second column
 DATA_LABEL = 'label' # Data column name for the label
 
 # Model training
-TRAIN_MODEL = True  # Train the model?
+TRAIN_MODEL = False  # Train the model?
 TRAIN_OPTIMIZED_MODEL = True # Train the optimized model?
+CREATE_HEATMAP = True # Create the heatmap for the optimized model?
+HEATMAP_H = 6 # Height of the heatmap (1 = 100px)
+HEATMAP_W = 6 # Width of the heatmap (1 = 100px)
 
 # Optimization methods:
 # 1: GridSearchCV
@@ -30,8 +33,8 @@ TRAIN_OPTIMIZED_MODEL = True # Train the optimized model?
 # 3: RandomizedSearchCV
 # 4: HalvingRandomSearchCV
 # 5: ALL (run all the above methods)
-OPTIMIZATION = 4
-CROSS_VALIDATIONS = 5  
+OPTIMIZATION = 2
+CROSS_VALIDATIONS = 4 # Number of cross-validations  
 NJOBS = -1 # Number of jobs to run in parallel. -1 = all processors.
 VERBOSE = 1 # Verbosity level for the optimization methods (0, 1, 2, 3)
 
@@ -40,6 +43,7 @@ VERBOSE = 1 # Verbosity level for the optimization methods (0, 1, 2, 3)
 RAW_DATA_PATH = os.path.join(BASE_DIR, RAW_DATA_DIR, RAW_DATA_FILENAME)
 PROCESSED_DATA_PATH = os.path.join(BASE_DIR, PROCESSED_DATA_DIR, PROCESSED_DATA_FILENAME)
 SCORES_PATH = os.path.join(OUTPUTS, SCORES)
+HEATMAP_PATH = os.path.join(OUTPUTS, 'heatmap')
 
 #Pipelines
 PIPELINE_PATH = os.path.join(OUTPUTS, 'pipeline')
@@ -50,9 +54,9 @@ RANDOM_STATE = 32
 
 # Parameter grid for GridSearchCV
 PARAM_GRID = {
-    "classifier__C": [0.01, 0.1, 1, 10, 100],
-    "classifier__gamma": ["scale", "auto", 0.001, 0.01, 0.1, 1],
+    "classifier__C": [0.1, 1, 10, 100],
+    "classifier__gamma": [0.001, 0.01, 0.1, 1],
     "classifier__kernel": ["poly", "rbf", "sigmoid"],
-    "classifier__degree": [2, 3, 4, 5],  
-    "classifier__coef0": [0.0, 0.1, 0.5, 1.0]  
+    "classifier__degree": [2, 4, 5],  
+    "classifier__coef0": [0.0, 0.5, 1.0]  
 }
