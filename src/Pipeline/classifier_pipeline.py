4a82b836003fe7956f5e25bbee495040f3513547
Kaapeli <kaapo.immonen@gmail.com>
2025-01-25 18:21:26 +0200

    Add heatmap generation after pipeline optimization

--- src/Pipeline/classifier_pipeline.py
+++ src/Pipeline/classifier_pipeline.py
@@ -4,9 +4,11 @@ from sklearn.svm import SVC
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import GridSearchCV, HalvingGridSearchCV, RandomizedSearchCV, HalvingRandomSearchCV
 from Pipeline.feature_union import create_feature_union
+from Analytics.heatmap import make_heatmap
 import joblib
 import config
 import time
+import os
 
 def create_pipeline():
     feature_union = create_feature_union()
@@ -23,17 +25,52 @@ def create_optimized_pipeline(param_grid, type, X, y):
     pipeline = create_pipeline()
     
     if type == 1:
-        grid_search = GridSearchCV(pipeline, return_train_score=True, param_grid=param_grid, cv=config.CROSS_VALIDATIONS, n_jobs=config.NJOBS, verbose=config.VERBOSE, error_score='raise')
+        grid_search = GridSearchCV(pipeline, 
+                                   return_train_score=True, 
+                                   param_grid=param_grid, 
+                                   cv=config.CROSS_VALIDATIONS, 
+                                   n_jobs=config.NJOBS, 
+                                   verbose=config.VERBOSE, 
+                                   error_score='raise'
+                                   )
         search_name = "GridSearchCV"
+        is_successive_halving = False
     elif type == 2:
-        grid_search = HalvingGridSearchCV(pipeline, return_train_score=True, param_grid=param_grid, cv=config.CROSS_VALIDATIONS, n_jobs=config.NJOBS, verbose=config.VERBOSE, error_score='raise', random_state=config.RANDOM_STATE)
+        grid_search = HalvingGridSearchCV(pipeline, 
+                                          return_train_score=True, 
+                                          param_grid=param_grid, 
+                                          cv=config.CROSS_VALIDATIONS, 
+                                          n_jobs=config.NJOBS, 
+                                          verbose=config.VERBOSE, 
+                                          error_score='raise', 
+                                          random_state=config.RANDOM_STATE,
+                                          )
         search_name = "HalvingGridSearchCV"
+        is_successive_halving = True
     elif type == 3:
-        grid_search = RandomizedSearchCV(pipeline, return_train_score=True, param_distributions=param_grid, cv=config.CROSS_VALIDATIONS, n_jobs=config.NJOBS, verbose=config.VERBOSE, error_score='raise', random_state=config.RANDOM_STATE)
+        grid_search = RandomizedSearchCV(pipeline, 
+                                         return_train_score=True, 
+                                         param_distributions=param_grid, 
+                                         cv=config.CROSS_VALIDATIONS, 
+                                         n_jobs=config.NJOBS, 
+                                         verbose=config.VERBOSE, 
+                                         error_score='raise', 
+                                         random_state=config.RANDOM_STATE, 
+                                        )
         search_name = "RandomizedSearchCV"
+        is_successive_halving = False
     elif type == 4:
-        grid_search = HalvingRandomSearchCV(pipeline, return_train_score=True, param_distributions=param_grid, cv=config.CROSS_VALIDATIONS, n_jobs=config.NJOBS, verbose=config.VERBOSE, error_score='raise', random_state=config.RANDOM_STATE)
+        grid_search = HalvingRandomSearchCV(pipeline, 
+                                            return_train_score=True, 
+                                            param_distributions=param_grid, 
+                                            cv=config.CROSS_VALIDATIONS, 
+                                            n_jobs=config.NJOBS, 
+                                            verbose=config.VERBOSE, 
+                                            error_score='raise', 
+                                            random_state=config.RANDOM_STATE
+                                            )
         search_name = "HalvingRandomSearchCV"
+        is_successive_halving = True
     else:
         return None, "No valid type provided", 0
     
@@ -42,5 +79,10 @@ def create_optimized_pipeline(param_grid, type, X, y):
     end_time = time.time()
     time_spent = end_time - start_time
     print(f"Time spent on {search_name}: {time_spent}")
+
+    joblib.dump(pipeline, os.path.join(config.PIPELINE_PATH, 'pipeline_' + search_name + '.pkl'))
+
+    if config.CREATE_HEATMAP:
+        make_heatmap(grid_search, time_spent, is_successive_halving, search_name)
     
     return grid_search, search_name, time_spent
